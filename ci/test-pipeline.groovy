8889b35375bf5e6e506a8cd16df9ee87e74158a0
Michael Mühlebach <michael@anduin.ch>
2023-08-21 16:09:32 +0200

    Adapt pipeline

--- ci/test-pipeline.groovy
+++ ci/test-pipeline.groovy
@@ -3,60 +3,59 @@ pipeline {
     kubernetes {
       inheritFrom "default"
       yaml '''
-        apiVersion: v1
-        kind: Pod
-        metadata:
-          labels:
-            com.joyrex2001.kubedock.service-account: kubedock
-            com.joyrex2001.kubedock.runas-user: 1000
-        spec:
-          containers:
-            - name: jnlp
-              securityContext:
-                runAsUser: 1000
-            - name: maven
-              workingDir: /home/jenkins/agent
-              image: maven:3.9.3-eclipse-temurin-17
-              command:
-                - sleep
-              args:
-                - 99d
-              securityContext:
-                runAsUser: 1000
-            - name: kubedock
-              image: joyrex2001/kubedock:0.12.0
-              command:
+    apiVersion: v1
+    kind: Pod
+    metadata:
+      labels:
+            com.joyrex2001.kubedock.runas-user: 1001130000
+    spec:
+      containers:
+        - name: jnlp
+          securityContext:
+                runAsUser: 1001130000
+        - name: maven
+          workingDir: /home/jenkins/agent
+          image: maven:3.9.3-eclipse-temurin-17
+          command:
+            - sleep
+          args:
+            - 99d
+          securityContext:
+                runAsUser: 1001130000
+        - name: kubedock
+          image: joyrex2001/kubedock:0.12.0
+          command:
                 - "/app/kubedock"
-              args:
-                - server
-                - '--runas-user'
-                - '1001130000'
+          args:
+            - server
+            - '--runas-user'
+            - '1001130000'
                 - '--reverse-proxy'
-              securityContext:
-                runAsUser: 1000
-              ports:
-                - containerPort: 2475
-              env:
-                - name: SERVICE_ACCOUNT
-                  value: kubedock
+          securityContext:
+                runAsUser: 1001130000
+          ports:
+            - containerPort: 2475
+          env:
+            - name: SERVICE_ACCOUNT
+              value: kubedock
         '''
     }
   }
   stages {
     stage('Run maven') {
       steps {
-        container('maven') {
-          echo POD_CONTAINER // displays 'maven'
+      container('maven') {
+        echo POD_CONTAINER // displays 'maven'
           // sh 'git clone https://github.com/baloise-incubator/testcontainers-k8s.git'
 
-          dir('examples/pure-testcontainers') {
+          dir('getting-started') {
             sh '''export TESTCONTAINERS_RYUK_DISABLED=true && \
                   export TESTCONTAINERS_CHECKS_DISABLE=true && \
                   export DOCKER_HOST=tcp://localhost:2475 && \
-                  mvn test -Dmaven.repo.local=/home/jenkins/agent/workspace/.m2'''
+                  export PGDATA=/home/jenkins/agent/pg-data && mkdir $PGDATA && \
+                  mvn test -Dmaven.repo.local=/home/jenkins/agent/.m2'''
           }
         }
       }
     }
-  }
 }
