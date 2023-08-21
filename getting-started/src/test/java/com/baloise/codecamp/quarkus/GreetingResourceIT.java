866a05f53906667a274078cf1a6cf87854cd70e8
Roland Seidel <b018095@X10063446.balgroupit.com>
2023-08-21 14:49:38 +0200

    add quarkus-test app #1

---
+++ getting-started/src/test/java/com/baloise/codecamp/quarkus/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.baloise.codecamp.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
